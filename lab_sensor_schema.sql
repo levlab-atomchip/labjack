5112692449c2be2b83f10c3fb4efb951f59b6fa2
Richard Turner <rwturner @ stanford.edu>
2013-12-05 18:26:22 -0800

    troubleshooting on database description
    this versions correctly creates the database

--- lab_sensor_schema.sql
+++ lab_sensor_schema.sql
@@ -3,39 +3,26 @@ CREATE TABLE system_states (
     name text
 );
 
-CREATE TABLE sensors (
+CREATE TABLE daqs (
     _id serial PRIMARY KEY,
-    name text NOT NULL,
-    unit text NOT NULL,
-    channel REFERENCES channels NOT NULL,
-    postprocessor REFERENCES postprocessors NOT NULL,
-    install_date timestamp DEFAULT CURRENT_TIMESTAMP,
-    remove_date timestamp CONSTRAINT remove_after_install CHECK (remove_date > install_date),
-    environmental boolean,
-    installed boolean,
-    faulted boolean,
-    description text
+    name text UNIQUE
 );
 
-PREPARE sensor_add(text, text, text, text, boolean, text) AS
-INSERT INTO sensors (name, unit, channel, postprocessor, install_date, remove_date, environmental, installed, faulted, description)
-SELECT $1, $2, c._id, p._id, DEFAULT, null, $5, false, false, $6
-FROM channels c, postprocessors p
-WHERE c.name = $3
-AND p.name = $4
-;
-
-CREATE TABLE states_sensors_map (
-    state_id REFERENCES system_states NOT NULL,
-    sensor_id REFERENCES sensors NOT NULL
+CREATE TABLE channel_types(
+    _id serial PRIMARY KEY,
+    name text UNIQUE
 );
 
-CREATE TABLE sensor_data (
-    sensor REFERENCES sensors NOT NULL ON DELETE RESTRICT,
-    time timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    value float NOT NULL,
-    system_state REFERENCES system_states
+CREATE TABLE drivers (
+    _id serial PRIMARY KEY,
+    name text UNIQUE
 );
+
+CREATE TABLE postprocessors(
+    _id serial PRIMARY KEY,
+    name text UNIQUE
+);
+
 
 CREATE TABLE runs (
     _id serial PRIMARY KEY,
@@ -44,17 +31,12 @@ CREATE TABLE runs (
     name text
 );
 
-CREATE TABLE annotations (
-    sensor REFERENCES sensors NOT NULL ON DELETE RESTRICT,
-    time timestamp NOT NULL,
-    description text NOT NULL
-);
 
 CREATE TABLE channels (
     _id serial PRIMARY KEY,
-    DAQ REFERENCES daqs,
-    type REFERENCES channel_types,
-    driver REFERENCES drivers,
+    DAQ integer REFERENCES daqs,
+    type integer REFERENCES channel_types,
+    driver integer REFERENCES drivers,
     name text,
     address text
 );
@@ -68,30 +50,54 @@ AND t.name = $2
 AND dr.name = $3
 ;
 
-CREATE TABLE daqs (
+CREATE TABLE sensors (
     _id serial PRIMARY KEY,
-    name text UNIQUE
+    name text NOT NULL,
+    unit text NOT NULL,
+    channel integer REFERENCES channels NOT NULL,
+    postprocessor integer REFERENCES postprocessors NOT NULL,
+    install_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    remove_date timestamp CONSTRAINT remove_after_install CHECK (remove_date > install_date),
+    environmental boolean,
+    installed boolean,
+    faulted boolean,
+    description text
 );
 
-CREATE TABLE channel_types(
-    _id serial PRIMARY KEY,
-    name text UNIQUE
+PREPARE sensor_add(text, text, text, text, boolean, text) AS
+INSERT INTO sensors (name, unit, channel, postprocessor, remove_date, environmental, installed, faulted, description)
+SELECT $1, $2, c._id, p._id, null, $5, false, false, $6
+FROM channels c, postprocessors p
+WHERE c.name = $3
+AND p.name = $4
+;
+
+CREATE TABLE states_sensors_map (
+    state_id integer REFERENCES system_states NOT NULL,
+    sensor_id integer REFERENCES sensors NOT NULL
 );
 
-CREATE TABLE drivers (
-    _id serial PRIMARY KEY,
-    name text UNIQUE,
+CREATE TABLE sensor_data (
+    sensor integer REFERENCES sensors NOT NULL,
+    time timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    value float NOT NULL,
+    system_state integer REFERENCES system_states
 );
 
-CREATE TABLE postprocessors(
-    _id serial PRIMARY KEY,
-    name text UNIQUE,
+CREATE TABLE annotations (
+    sensor integer REFERENCES sensors NOT NULL,
+    time timestamp NOT NULL,
+    description text NOT NULL
 );
 
-INSERT INTO channel_types name VALUES 'AIN24';
-INSERT INTO daqs name VALUES 'MIMIR_Labjack';
-INSERT INTO postprocessors name VALUES 'raw_result';
-INSERT INTO drivers name VALUES 'AIN24_Driver';
+
+
+
+
+INSERT INTO channel_types (name) VALUES ('AIN24');
+INSERT INTO daqs (name) VALUES ('MIMIR_Labjack');
+INSERT INTO postprocessors (name) VALUES ('raw_result');
+INSERT INTO drivers (name) VALUES ('AIN24_Driver');
 
 EXECUTE channel_add('MIMIR_Labjack', 'AIN24', 'AIN24_Driver', 'MIMIR_AIN0', '0');
 EXECUTE sensor_add('Test Photodiode', 'Volts', 'MIMIR_AIN0', 'raw_result', true, 'DET36A Photodiode for testing data logging');
